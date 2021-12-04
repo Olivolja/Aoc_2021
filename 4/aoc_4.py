5520fa3696f08c3dbb74a6adeddf738c3645c073
Adam Oliv <adamwoliv@gmail.com>
2021-12-04 12:17:28 +0100

    day 4 part 1

---
+++ 4/aoc_4.py
@@ -0,0 +1,68 @@
+import math
+input = open("4/input.txt").read().split("\n")
+
+# print([(type(x),x) for x in input[2]]) 
+
+def part1(input):
+    draws = input[0].split(",")
+    del input[:2] #input is now "" ,lines in board, ""
+
+    boards = generate_boards(input)
+
+    for number in draws:
+
+        for board_i in range(len(boards)):
+            for row in range(len(boards[board_i])):
+                for e in range(len(boards[board_i][row])):
+                    #check number and replase with x if exist
+                    if boards[board_i][row][e] == number:
+                        # print("ok")
+                        boards[board_i][row][e]  = "x"
+                    #check if winner exist
+                    winning_table = check_win(boards)
+                    if(winning_table):
+
+                        return sum_board(boards[winning_table] * int(number))
+
+    print(boards)
+    score = 0
+    return score
+
+
+def sum_board(board):
+    sum = 0
+    for row_i, row in enumerate(board):
+        for e_i, e in enumerate (row):
+            if (e != "x"):
+                sum += int(e)
+    return sum
+
+def generate_boards(boardlines):
+    boards = []
+    currentboard = []
+    for line in input:
+        if(line ==""):
+            boards.append(currentboard)
+            currentboard = []
+        else:
+            l = line.split(" ")
+            l = [x for x in l if x!=""]
+            currentboard.append(l)
+    return boards
+
+def check_win(boards):
+    winning_table = False
+    for table_index, table in enumerate(boards):
+        for row_i, row in enumerate(table):
+            if row == ["x", "x", "x", "x", "x"]:
+                winning_table = table_index
+
+    for table_index, table in enumerate(boards):
+        trasnposed_table = list(map(list, zip(*table)))
+        for row_i, row in enumerate(trasnposed_table):
+            if row == ["x", "x", "x", "x", "x"]:
+                winning_table = table_index
+    return winning_table
+
+
+print(part1(input))
